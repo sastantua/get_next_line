905ad307eb2927c9349c50608626b02a8890a7fa
nicovrg <nicolasvergne88@gmail.com>
2019-05-15 21:24:27 +0200

    leaks removed

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: nivergne <nivergne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/15 17:10:09 by nivergne          #+#    #+#             */
-/*   Updated: 2018/11/29 01:35:49 by nivergne         ###   ########.fr       */
+/*   Updated: 2019/05/15 19:00:08 by nivergne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ static int	find_newline(char *store)
 		return (-1);
 }
 
-static char	*join_free(char *store, char *buff)
+static char	*join_free(char *store, char buff[BUFF_SIZE])
 {
 	int		i;
 	int		j;
@@ -46,19 +46,20 @@ static char	*join_free(char *store, char *buff)
 	return (join);
 }
 
-static int	assign_line(char **store, char **buff, char **line)
+static int	assign_line(char **store, char buff[BUFF_SIZE], char **line)
 {
 	int		index;
 	char	*tmp;
 
-	*store = join_free(*store, *buff);
+	*store = join_free(*store, buff);
 	index = find_newline(*store);
 	if (index != -1)
 	{
 		tmp = *store;
 		*line = ft_strsub(*store, 0, index);
-		*store = ft_strdup(*store + index + 1);
-		free(tmp);
+		tmp = ft_strdup(*store + index + 1);
+		ft_strdel(store);
+		*store = tmp;
 		return (1);
 	}
 	return (0);
@@ -68,21 +69,19 @@ int			get_next_line(const int fd, char **line)
 {
 	int				ret;
 	int				ret_2;
-	char			*buff;
-	static char		*store[OPEN_MAX];
+	char			buff[BUFF_SIZE];
+	static char		*store[10000];
 
-	buff = ft_strnew(BUFF_SIZE);
 	if (!line || BUFF_SIZE <= 0 || (ret = read(fd, buff, 0)) < 0)
 		return (-1);
 	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
 	{
-		ret_2 = assign_line(&store[fd], &buff, line);
-		ft_strdel(&buff);
+		ret_2 = assign_line(&store[fd], buff, line);
+		bzero((void *)buff, BUFF_SIZE);
 		if (ret_2 == 1)
 			return (1);
-		buff = ft_strnew(BUFF_SIZE);
 	}
-	if (assign_line(&store[fd], &buff, line) == 1)
+	if (assign_line(&store[fd], buff, line) == 1)
 		return (1);
 	else if (ft_strlen(store[fd]) > 0)
 	{
